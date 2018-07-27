5f8eed8b359dbd27440b8ee0a8d562a117048be3
sqzaman <sqzaman@gmail.com>
2018-07-27 01:50:44 -0500

    additional stuff

---
+++ backend/iShopOAuth2Service/src/main/java/ishop/IShopOAuth2ServiceApplication.java
@@ -0,0 +1,12 @@
+package ishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IShopOAuth2ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IShopOAuth2ServiceApplication.class, args);
+	}
+}
