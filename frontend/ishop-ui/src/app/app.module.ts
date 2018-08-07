44dee78514c042502492cf15941165bb2ec2256c
sqzaman <sqzaman@gmail.com>
2018-08-07 10:05:37 -0500

    components for ishop has been added

--- frontend/ishop-ui/src/app/app.module.ts
+++ frontend/ishop-ui/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ContentComponent } from './content/content.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SidebarComponent,
+    ContentComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
