adf017bb78ee5a48be7f3c33fc39378f26678418
Mohammed Rezki <mrezki@mum.edu>
2018-08-07 15:07:36 -0500

    add authentication module

---
+++ frontend/ishop-ui/src/app/modules/authentication/authentication.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignupComponent } from './components/signup/signup.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SignupComponent]
+})
+export class AuthenticationModule { }
