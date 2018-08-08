deb11eb7c86dd4205ee59969d86c8affb24bafd7
sqzaman <sqzaman@gmail.com>
2018-08-08 15:25:23 -0500

    updated

--- frontend/ishop-ui/src/app/app.module.ts
+++ frontend/ishop-ui/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './modules/home/home.module';
 import { ProductsModule } from './modules/products/products.module';
@@ -20,6 +21,7 @@ import { FooterComponent } from './shared/components/footer/footer.component';
   imports: [
     BrowserModule,
     HttpModule,
+    HttpClientModule,
     AppRoutingModule,
     AuthenticationModule,
     HomeModule,
