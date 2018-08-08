14b2e476b9b3133f949307539d7eaf67b74cdc5c
Mohammed Rezki <mrezki@mum.edu>
2018-08-08 07:32:04 -0500

    add services for products

---
+++ frontend/ishop-ui/src/app/modules/products/services/category_services/category.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Globals } from '../../../../shared/config/globals.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+
+  constructor(private http: HttpClient, private globals: Globals) { }
+
+  getCategories() {
+    return this.http.get(this.globals.BASE_API_URL+"product/category/get");
+  }
+
+}
